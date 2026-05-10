9f310cc21534351982a0077715c555bca9ab87fd
sanikamane-cse <msanika487@gmail.com>
2026-05-10 08:03:30 +0530

    Fix cookie sameSite for production

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -8,7 +8,7 @@ const generateToken = (res, userId) => {
   res.cookie("token", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 
