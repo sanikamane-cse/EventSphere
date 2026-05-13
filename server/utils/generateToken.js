51447f4638079527f235c6af8b47f7dc98702823
sanikamane-cse <msanika487@gmail.com>
2026-05-13 09:56:42 +0530

    Fix cookie sameSite none for mobile

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -7,8 +7,8 @@ const generateToken = (res, userId) => {
 
   res.cookie("token", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+    secure: true,
+    sameSite: "none",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 
